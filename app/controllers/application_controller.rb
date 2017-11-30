0a5ccffd701b1dd6b626a0961ab2c1028121d938
Jonas <jonas.preisler@gmail.com>
2017-11-30 08:39:54 +0800

    slettede authenticate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  before_action :authenticate_user!
+  skip_before_filter :verify_authenticity_token
 end
