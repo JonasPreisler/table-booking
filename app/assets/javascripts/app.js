3c6d0e274504820c486c530ca2c74510a984bb76
Jonas <jonas.preisler@gmail.com>
2017-12-11 14:24:38 +0800

    something

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1401,3 +1401,4 @@ $(document).ready(function() {
         }
     });
 });
+
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
+  mount_uploader :image, ImageUploader
+  attr_accessor :image
   belongs_to :customer
   belongs_to :staff
   has_one :measurement
