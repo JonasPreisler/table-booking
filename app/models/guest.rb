49e33237b813a56eea1a70885eddb93b121c6059
Jonas <jonas.preisler@gmail.com>
2017-12-14 15:03:41 +0800

    fffee

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,2 +1,7 @@
 class Guest < ApplicationRecord
+    belongs_to :guestlist
+
+    def guestlist_objects
+      Guestlist.where(id: self.guestlist_id.reject(&:empty?)).map(&:id).join(",")
+    end
 end
