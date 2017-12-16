d542aa82882dace833ce83d52795fbeb2adb371c
Jonas <jonas.preisler@gmail.com>
2017-12-17 00:05:25 +0800

    jaja ok

--- app/models/guestlist.rb
+++ app/models/guestlist.rb
@@ -1,7 +1,6 @@
 class Guestlist < ApplicationRecord
   has_many :guests
   belongs_to :event
-  belongs_to :ambassador
 
     def event_objects
       Event.where(id: self.event.reject(&:empty?)).map(&:title).join(",")
