b5fd648e590b3955148d3447cf456cc32b28abd8
Jonas <jonas.preisler@gmail.com>
2017-12-17 19:52:47 +0800

    yep yep

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   skip_before_filter :verify_authenticity_token
-
+  before_action :authenticate_user!
+  
   def authorize_user
       redirect_to :back, status: 401 unless current_user
       #redirects to previous page
