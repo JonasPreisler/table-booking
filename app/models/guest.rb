6c97717d15c3971fb3f7e8b8f07721a7a792cac5
Jonas <jonas.preisler@gmail.com>
2017-12-20 20:44:34 +0800

    ok

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,7 +1,7 @@
 class Guest < ApplicationRecord
     belongs_to :guestlist
     belongs_to :ambassador
-    validates :typ_ambassador_name, presence: true
+    validates :ambassador, presence: true
 
     def guestlist_objects
       Guestlist.where(id: self.guestlist_id.reject(&:empty?)).map(&:id).join(",")
