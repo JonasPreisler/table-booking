96e3821f7eb7f262b1106ea2a2f1866abeffed24
Jonas <jonas.preisler@gmail.com>
2018-02-12 08:19:09 +0800

    error messages try19

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -22,6 +22,21 @@ class CallbackController < ApplicationController
     render "received_data"
   end
 
+  def sent_data
+    therequest = request.body.read
+    data = JSON.parse(therequest)
+    enteries = data["entry"]
+    enteries.each do |entry|
+      entry["messaging"].each do |messaging|
+        sender = messaging ["sender"]["id"]
+        text = messaging["message"]["text"]
+        myjson = {"recipient": {"id": "#{sender}"}, "message": {"text": "text"}}
+        puts HTTP.post(url, json: myjson)
+      end
+    end
+    render "received_data"
+  end
+
   def url
     "https://graph.facebook.com/v2.6/me/messages?access_token=EAACJIOqpB24BAMOhuekkbZB6aI6RYCxnQDftExY40kClUZC5RHEzZCnkqD4msEzWFmmaT1NarymCgFjD1kDqMIiOZAo2b3cjqc8FU3jAq73IZBXjAAeLtaPQb9FDBssPP560XawCTZA1xAFCULeBAywjEj3lBH4VlEU0vPgQ1rOFYGEF6QYRqw"
   end
